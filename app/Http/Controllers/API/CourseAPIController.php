43ebd580be3189234c4ea4177438f6091588a683
Brian Chebon <brynnlamura@gmail.com>
2024-02-16 12:24:06 +0300

    added the APIController

---
+++ app/Http/Controllers/API/CourseAPIController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class CourseAPIController extends Controller
+{
+    //GET, PUT, POST & DELETE
+
+    //Adding, POST
+    public function store() {
+        
+    }
+}
